9648c9a5e1de2f8dd0d3fd19c3624318df216ae3
Adam Miller <admiller@redhat.com>
2025-07-10 16:52:37 -0500

    add formatting git hook
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- test_formatting.py
+++
@@ -1,9 +0,0 @@
-# This file has intentionally bad formatting to test the pre-commit hook
-def badly_formatted_function(    x,y,    z):
-    return x+y+z
-
-
-class BadlyFormattedClass:
-    def __init__(self,name,value):
-        self.name=name
-        self.value=value
